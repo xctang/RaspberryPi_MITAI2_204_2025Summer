c23fce537cb9e1e89853d6ab99da28ad24eb9858
xctang <xctang09@gmail.com>
2025-07-23 20:22:44 -0500

    Create 204_DHT11_GPIO_Lab.py

---
+++ Lecture08/204_DHT11_GPIO_Lab.py
@@ -0,0 +1,32 @@
+#This is a program to display Temp and Humidity on MIT App
+
+import time
+from flask import Flask, render_template
+import RPi.GPIO as GPIO
+import adafruit_dht
+from board import *
+
+# GPIO17
+SENSOR_PIN = D17
+
+app = Flask(__name__)
+
+@app.route('/', method=['GET','POST'])
+
+def get_data():
+    
+    dht11 = adafruit_dht.DHT11(SENSOR_PIN, use_pulseio=False)
+    temp = dht11.temperature
+    hum = dht11.humidity
+    tempint = int(temp)
+    humint = int(hum)
+    if tempint <10:
+        datat = "0" + str(tempint)
+    else:
+        datat = str(tempint)
+        
+    datath = datat + "," + str(humint)
+    return(datath)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0', use_relaoder=False)
